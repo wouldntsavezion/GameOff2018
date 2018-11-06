bbc51499a68324afc404a2109cf822b4681d0249
Nicola Baribeau <n_baribeau@hotmail.com>
2018-11-05 19:33:28 -0500

    Adding base project + .gitignore

---
+++ Assets/Scripts/SceneMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMenu : MonoBehaviour {
+    public void onStartClick() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void onExitClick() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit ();
+        #endif
+    }
+}
